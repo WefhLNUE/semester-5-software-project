7c94e8366a300b5f47b4c25c2c58e9e161f3da73
Aster-00 <ammarabdelkhalek021@gmail.com>
2025-11-15 20:04:36 +0200

    Add MongoDB connection setup and update dependencies in package.json and package-lock.json

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import connectDB from './db';
 
 async function bootstrap() {
+  // Connect to MongoDB
+  await connectDB();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT ?? 3000;
+  await app.listen(port);
+  console.log(`🚀 Application is running on: http://localhost:${port}`);
 }
 bootstrap();
