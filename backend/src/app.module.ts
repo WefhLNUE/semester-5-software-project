36ea65029e79383c5b4b7203a016b4436dbdfdad
kanzyhany <kanzyhany06@gmail.com>
2025-12-19 05:04:50 +0200

    added module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { AppService } from './app.service';
 import { RecruitmentModule } from './recruitment/recruitment.module';
 import { LeavesModule } from './leaves/leaves.module';
 import { AuthModule } from './auth/auth.module';
+import { OrganizationStructureModule } from './organization-structure/organization-structure.module';
 import { EmployeeProfile, EmployeeProfileSchema } from './employee-profile/Models/employee-profile.schema';
 import * as dotenv from 'dotenv';
 dotenv.config();
@@ -22,10 +23,11 @@ dotenv.config();
       { name: EmployeeProfile.name, schema: EmployeeProfileSchema },
     ]),
     AuthModule,
+    OrganizationStructureModule,
     RecruitmentModule,
     LeavesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
